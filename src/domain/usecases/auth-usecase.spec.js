2ebcfd868fb079f6f386289108901bb5f26e45f8
Leo Buchner <developerdelphi@gmail.com>
2021-05-20 21:34:13 -0300

    ensure AuthUsecase throw if not password is provided

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,7 +1,8 @@
 const { MissingParamError } = require('../../utils/errors')
 class AuthUseCase {
-  async auth (email) {
+  async auth (email, password) {
     if (!email) throw new MissingParamError('email')
+    if (!password) throw new MissingParamError('password')
   }
 }
 
@@ -11,5 +12,12 @@ describe('Auth UseCase', () => {
     const promise = sut.auth()
 
     expect(promise).rejects.toThrow(new MissingParamError('email'))
+  })
+
+  it('Should throw if not password is provided', async () => {
+    const sut = new AuthUseCase()
+    const promise = sut.auth('any_email@email.com')
+
+    expect(promise).rejects.toThrow(new MissingParamError('password'))
   })
 })
