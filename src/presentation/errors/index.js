45c6068c87fd50ec6294ea21ac4f2ccbddaeed99
Leo Buchner <developerdelphi@gmail.com>
2021-05-20 21:27:22 -0300

    move generic erros to utils folder erros

--- src/presentation/errors/index.js
+++ src/presentation/errors/index.js
@@ -1,11 +1,7 @@
-const MissingParamError = require('./missing-param-error')
 const InternalServerError = require('../errors/internal-server-error')
-const InvalidParamError = require('../errors/invalid-param-error')
 const UnauthorizedError = require('../errors/unauthorized-error')
 
 module.exports = {
-  MissingParamError,
   InternalServerError,
-  InvalidParamError,
   UnauthorizedError
 }
